a06e2b88e099156835c1ccfde9e44a9a63fc71ba
kxxt <rsworktech@outlook.com>
2021-11-24 18:55:22 +0800

    Mock Table Of Contents.

--- src/components/TableOfContents.vue
+++ src/components/TableOfContents.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="is-hidden-touch is-narrow">
-    Table of Contents
-  </div>
+  <aside class="menu is-hidden-touch is-narrow">
+    <p class="subtitle">目录</p>
+    <p class="menu-label">第一部分</p>
+    <ul class="menu-list">
+      <li><a>Hello</a></li>
+      <li><a>World</a></li>
+    </ul>
+    <p class="menu-label">第二部分</p>
+    <ul class="menu-list">
+      <li><a>Hello</a></li>
+      <li><a>World</a></li>
+      <li><a>AAAAA</a></li>
+    </ul>
+    <p class="menu-label">第三部分</p>
+    <ul class="menu-list">
+      <li><a>Hello</a></li>
+      <li><a>World</a></li>
+      <li><a>AAAAA</a></li>
+    </ul>
+  </aside>
 </template>
 
 <script>
