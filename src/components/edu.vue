698b97c77370959f2721ddafbc1c9c847644ca85
kxxt <rsworktech@outlook.com>
2021-11-30 16:58:47 +0800

    2 column chart.

--- src/components/edu.vue
+++ src/components/edu.vue
@@ -1,18 +1,21 @@
 <template>
-  <v-chart class="chart" :autoresize="true" :option="option" />
+  <div class="columns">
+    <v-chart class="chart column" :autoresize="true" :option="option2010"/>
+    <v-chart class="chart column" :autoresize="true" :option="option2018"/>
+  </div>
 </template>
 
 <script>
-import { use } from "echarts/core";
-import { CanvasRenderer } from "echarts/renderers";
-import { PieChart } from "echarts/charts";
+import {use} from "echarts/core";
+import {CanvasRenderer} from "echarts/renderers";
+import {PieChart} from "echarts/charts";
 import {
   TitleComponent,
   TooltipComponent,
   LegendComponent,
 } from "echarts/components";
 import VChart from "vue-echarts";
-import { ref, defineComponent } from "vue";
+import {ref, defineComponent} from "vue";
 
 use([
   CanvasRenderer,
@@ -28,15 +31,11 @@ export default defineComponent({
     VChart,
   },
   setup: () => {
-    const option = ref({
+    const option2010 = ref({
       title: [
         {
           text: "2010 Education",
-          left: "23%",
-        },
-        {
-            text:"2018 Education",
-            left:'67%'
+          left: "center",
         },
       ],
       tooltip: {
@@ -60,14 +59,14 @@ export default defineComponent({
           name: "Education level",
           type: "pie",
           radius: "55%",
-          center: ["28%", "60%"],
+          //center: ["28%", "60%"],
           data: [
-            { value: 4, name: "博士" },
-            { value: 48, name: "硕士" },
-            { value: 870, name: "大学本科" },
-            { value: 4532, name: "高中/中专/技校/职高" },
-            { value: 6973, name: "小学" },
-            { value: 9920, name: "文盲/半文盲" },
+            {value: 4, name: "博士"},
+            {value: 48, name: "硕士"},
+            {value: 870, name: "大学本科"},
+            {value: 4532, name: "高中/中专/技校/职高"},
+            {value: 6973, name: "小学"},
+            {value: 9920, name: "文盲/半文盲"},
           ],
           emphasis: {
             itemStyle: {
@@ -77,18 +76,43 @@ export default defineComponent({
             },
           },
         },
+      ]
+    });
+    const option2018 = ref({
+      title: [
         {
-            name: "2018 Education level",
+          text: "2018 Education",
+          left: 'center'
+        },
+      ],
+      tooltip: {
+        trigger: "item",
+        formatter: "{a} <br/>{b} : {c} ({d}%)",
+      },
+      legend: {
+        orient: "vertical",
+        left: "left",
+        data: [
+          "博士",
+          "硕士",
+          "大学本科",
+          "高中/中专/技校/职高",
+          "小学",
+          "文盲/半文盲",
+        ],
+      },
+      series: [
+        {
+          name: "2018 Education level",
           type: "pie",
           radius: "55%",
-          center: ["72%", "60%"],
           data: [
-            { value: 7, name: "博士" },
-            { value: 112, name: "硕士" },
-            { value: 1335, name: "大学本科" },
-            { value: 3058, name: "高中/中专/技校/职高" },
-            { value: 3598, name: "小学" },
-            { value: 2848, name: "文盲/半文盲" },
+            {value: 7, name: "博士"},
+            {value: 112, name: "硕士"},
+            {value: 1335, name: "大学本科"},
+            {value: 3058, name: "高中/中专/技校/职高"},
+            {value: 3598, name: "小学"},
+            {value: 2848, name: "文盲/半文盲"},
           ],
           emphasis: {
             itemStyle: {
@@ -96,13 +120,12 @@ export default defineComponent({
               shadowOffsetX: 0,
               shadowColor: "rgba(0, 0, 0, 1)",
             }
-            }
+          }
         }
-       ]
+      ]
     });
-    // 怎么规定颜色
 
-    return { option };
+    return {option2010, option2018};
   },
 });
 </script>
