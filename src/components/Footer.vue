977a7e3a8d8ca9d8069458026801d00450db8e5c
kxxt <rsworktech@outlook.com>
2021-12-05 16:29:16 +0800

    Translate license to chinese.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,8 @@
   <footer class="footer">
     <div class="content has-text-centered">
       <p>
-        The website content is licensed <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+        网站内容共享协议: <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh">署名-非商业性使用-相同方式共享 4.0 国际 (CC
+        BY-NC-SA 4.0)</a>
       </p>
       <p>Github: <a href="https://github.com/lie-flat/cfps-report">https://github.com/lie-flat/cfps-report</a></p>
       <p></p>
