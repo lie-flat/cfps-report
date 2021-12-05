827b9ff2e62028130b4efbdad78f3b805242830c
kxxt <rsworktech@outlook.com>
2021-12-05 22:26:15 +0800

    Format file.

--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,17 @@ const ImageminPlugin = require('imagemin-webpack-plugin').default
 const ImageminMozjpeg = require('imagemin-mozjpeg')
 
 module.exports = {
-  configureWebpack: {
-    plugins: [
-      new ImageminPlugin({
-        disable: process.env.NODE_ENV !== 'production',
-        test: /\.(jpe?g|png|gif|svg)$/i,
+    configureWebpack: {
         plugins: [
-          ImageminMozjpeg({
-            quality: 70
-          })
+            new ImageminPlugin({
+                disable: process.env.NODE_ENV !== 'production',
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                plugins: [
+                    ImageminMozjpeg({
+                        quality: 70
+                    })
+                ]
+            }),
         ]
-      })
-    ]
-  }
+    }
 }
