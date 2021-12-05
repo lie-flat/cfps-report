9e043763608e9b253427b9db3b4195a9d8d173c8
kxxt <rsworktech@outlook.com>
2021-12-05 16:29:03 +0800

    Add github link to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,6 +4,8 @@
       <p>
         The website content is licensed <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
       </p>
+      <p>Github: <a href="https://github.com/lie-flat/cfps-report">https://github.com/lie-flat/cfps-report</a></p>
+      <p></p>
     </div>
   </footer>
 </template>
