a9254bcf9fd459f507ffc782947f82d2497b64db
Sunlight-qwq <2802655605@qq.com>
2021-12-12 22:22:19 +0800

    fix bug

--- src/components/charts/PlantAnimalExpense.vue
+++ src/components/charts/PlantAnimalExpense.vue
@@ -172,10 +172,6 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-#example-section {
-  background-image: url("../../assets/header-bg.jpg");
-}
-
 .chart {
   height: 350px;
 }
