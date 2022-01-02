36b14399db536bf2d4de4bf75446a4999bfa954f
kxxt <rsworktech@outlook.com>
2022-01-02 17:12:29 +0800

    Add link to kxxt's personal website.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,8 +6,11 @@
         BY-NC-SA 4.0)</a>
       </p>
       <p>Github: <a href="https://github.com/lie-flat/cfps-report">https://github.com/lie-flat/cfps-report</a></p>
-      <p>数据分析代码：<a href="https://github.com/lie-flat/cfps-analyze/tree/master/nbs">https://github.com/lie-flat/cfps-analyze/tree/master/nbs</a></p>
+      <p>数据分析代码：<a href="https://github.com/lie-flat/cfps-analyze/tree/master/nbs">https://github.com/lie-flat/cfps-analyze/tree/master/nbs</a>
+      </p>
       <p>部分讲解幻灯片：<a href="https://github.com/lie-flat/slides">https://github.com/lie-flat/slides</a></p>
+      <p class="subtitle is-5">友情链接</p>
+      <p>kxxt 的个人网站：<a href="https://kxxt.vercel.app">https://kxxt.vercel.app</a></p>
     </div>
   </footer>
 </template>
